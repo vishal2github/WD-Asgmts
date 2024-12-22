612cda58164bcb705809adc80930a5f9dc79c10f
Vishal Bhardwaj <vishalbhardwajformal@gmail.com>
2024-12-22 19:38:05 +0530

    Added JS-Assignment-1

---
+++ JS-Assignment-1/js-asgmt-1.js
@@ -0,0 +1,12 @@
+// ASSIGNMENT 1
+
+let a = 5, b = 5002;
+
+function AddTwoNumbers(x, y) {
+    let sum = x + y;
+    return sum;
+}
+
+if((a >= 1) && (b <= 5001)) {
+    console.log(AddTwoNumbers(a, b));
+}
