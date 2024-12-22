61bb6db0c44bba393854bea339a634826a16bd77
Vishal Bhardwaj <vishalbhardwajformal@gmail.com>
2024-12-22 20:32:07 +0530

    Added JS-Assignment-7

---
+++ JS-Assignment-7/js-asgmt-7.js
@@ -0,0 +1,14 @@
+// ASSIGNMENT 7
+
+let a = 15, b = 5;
+
+function Multiply_two_numbers(x, y) {
+    let res = x * y;
+    console.log(res);
+}
+
+if((a >= 1) && (b <= 301)) {
+    Multiply_two_numbers(a, b);
+} else {
+    console.log("Error: Constraints out of bound!");
+}
